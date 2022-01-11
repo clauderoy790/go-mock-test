731666c550b678ce6c2d8c2e6dbfebf5e9b1304c
claude_roy <Claude.Roy@mheducation.com>
2022-01-11 10:39:47 -0600

    first commit

---
+++ go.mod
@@ -0,0 +1,5 @@
+module mock-test.com/test
+
+go 1.17
+
+require github.com/clauderoy790/go-external-lib v1.0.1 // indirect
---
+++ main.go
@@ -0,0 +1,11 @@
+package main
+
+import (
+	"fmt"
+"github.com/clauderoy790/go-external-lib"
+)
+
+func main() {
+
+	fmt.Println(platform.GetString("test"))
+}
